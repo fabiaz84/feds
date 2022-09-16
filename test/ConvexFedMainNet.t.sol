d0c9307843896630543aff55832352a228ead40a
08xmt <markus.08xmt@gmail.com>
2022-09-16 23:08:09 +0800

    Modify convex-fed tests to use fraxBP as target

--- test/ConvexFedMainNet.t.sol
+++ test/ConvexFedMainNet.t.sol
@@ -19,13 +19,13 @@ interface IMinted is IERC20 {
 contract ConvexFedTest is DSTest {
     Vm internal constant vm = Vm(HEVM_ADDRESS);
     IConvexBooster public convexBooster = IConvexBooster(0xF403C135812408BFbE8713b5A23a04b3D48AAE31);
-    IConvexBaseRewardPool public baseRewardPool = IConvexBaseRewardPool(0x835f69e58087E5B6bffEf182fe2bf959Fe253c3c);
-    IMetaPool public crvPool = IMetaPool(0xAA5A67c256e27A5d80712c51971408db3370927D);
+    IConvexBaseRewardPool public baseRewardPool = IConvexBaseRewardPool(0x0404d05F3992347d2f0dC3a97bdd147D77C85c1c);
+    IMetaPool public crvPool = IMetaPool(0xE57180685E3348589E9521aa53Af0BCD497E884d);
     ConvexFed public convexFed;
     IMinted public dola = IMinted(0x865377367054516e17014CcdED1e7d814EDC9ce4);
     IERC20 public crv = IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
     IERC20 public cvx = IERC20(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
-    IERC20 public crv3 = IERC20(0x6c3F90f043a72FA612cbac8115EE7e52BDe6E490);
+    IERC20 public fraxBP = IERC20(0x3175Df0976dFA876431C2E9eE6Bc45b65d3473CC);
     address public gov = 0x926dF14a23BE491164dCF93f4c468A50ef659D5B;
     address public chair = address(0xB);
     address public dolaFaucet = address(0xF);
@@ -59,7 +59,7 @@ contract ConvexFedTest is DSTest {
     }
 
     function testExpansion_succeed_whenExpandedWithinAcceptableSlippage(uint amount) public {
-        vm.assume(amount < 10_000_000 * 10**18);
+        vm.assume(amount < 1_000_000 * 10**18);
         vm.assume(amount > 10**18);
         uint initialDolaSupply = convexFed.dolaSupply();
         uint initialCrvLpSupply = convexFed.crvLpSupply();
@@ -83,7 +83,7 @@ contract ConvexFedTest is DSTest {
     }
 
     function testContraction_succeed_whenContractedWithinAcceptableSlippage(uint amount) public {
-        vm.assume(amount < 10_000_000 * 10**18);
+        vm.assume(amount < 500_000 * 10**18);
         vm.assume(amount > 10**18);
         vm.prank(chair);
         convexFed.expansion(amount*2);
@@ -109,11 +109,11 @@ contract ConvexFedTest is DSTest {
     }
 
     function testContraction_succeed_whenContractedWithProfit(uint amount) public {
-        vm.assume(amount < 10_000_000 * 10**18);
+        vm.assume(amount < 1_000_000 * 10**18);
         vm.assume(amount > 10**18);
         vm.prank(chair);
         convexFed.expansion(amount);
-        washTrade(50_000_000 ether, 100);
+        washTrade(1_000_000 ether, 100);
         uint initialDolaSupply = convexFed.dolaSupply();
         uint initialDolaTotalSupply = dola.totalSupply();
         uint initialCrvLpSupply = convexFed.crvLpSupply();
@@ -152,8 +152,8 @@ contract ConvexFedTest is DSTest {
 
     function testContractAll_succeed_whenContractedWithProfit() public {
         vm.prank(chair);
-        convexFed.expansion(1000_000 ether);
-        washTrade(10_000_000 ether, 100);
+        convexFed.expansion(100_000 ether);
+        washTrade(100_000 ether, 100);
         uint initialDolaSupply = convexFed.dolaSupply();
         uint initialDolaTotalSupply = dola.totalSupply();
         uint initialGovDola = dola.balanceOf(gov);
@@ -171,7 +171,7 @@ contract ConvexFedTest is DSTest {
     }
 
     function testFailContraction_fail_whenContractedOutsideAcceptableSlippage() public {
-        uint amount = 1000_000 ether;
+        uint amount = 100_000 ether;
 
         vm.startPrank(chair);
         convexFed.expansion(amount);
@@ -181,7 +181,7 @@ contract ConvexFedTest is DSTest {
     }
 
     function testFailContractAll_fail_whenContractedOutsideAcceptableSlippage() public {
-        uint amount = 1000_000 ether;
+        uint amount = 100_000 ether;
 
         vm.startPrank(chair);
         convexFed.expansion(amount);
@@ -192,7 +192,7 @@ contract ConvexFedTest is DSTest {
 
     function testTakeProfit_NoProfit_whenCallingWhenUnprofitable() public {
         vm.startPrank(chair);
-        convexFed.expansion(1000_000 ether);
+        convexFed.expansion(100_000 ether);
         uint initialCvx = cvx.balanceOf(gov);
         uint initialCvxCrv = crv.balanceOf(gov);
         uint initialCrvLpSupply = convexFed.crvLpSupply();
@@ -208,7 +208,7 @@ contract ConvexFedTest is DSTest {
 
     function testTakeProfit_IncreaseGovCrvCvxBalance_whenCallingWithoutHarvestLpFlag() public {
         vm.startPrank(chair);
-        convexFed.expansion(1000_000 ether);
+        convexFed.expansion(100_000 ether);
         uint initialCvx = cvx.balanceOf(gov);
         uint initialCvxCrv = crv.balanceOf(gov);
         uint initialCrvLpSupply = convexFed.crvLpSupply();
@@ -226,10 +226,10 @@ contract ConvexFedTest is DSTest {
 
     function testTakeProfit_IncreaseGovDolaBalance_whenCallingWithHarvestLpFlag() public {
         vm.prank(chair);
-        convexFed.expansion(1000_000 ether);
+        convexFed.expansion(100_000 ether);
         uint initialCrvLpSupply = convexFed.crvLpSupply();
         uint initialGovDola = dola.balanceOf(gov);
-        uint input = 10_000_000 ether;
+        uint input = 1000_000 ether;
         washTrade(input, 100);
         vm.prank(chair);
         convexFed.takeProfit(true);
@@ -303,8 +303,8 @@ contract ConvexFedTest is DSTest {
 
     function testTakeProfit_FailWithOnlyChair_whenCalledByOtherAddress() public {
         vm.prank(chair);
-        convexFed.expansion(1000_000 ether);
-        washTrade(10_000_000 ether, 100);
+        convexFed.expansion(100_000 ether);
+        washTrade(1_000_000 ether, 100);
  
         vm.prank(dolaFaucet);
         vm.expectRevert("ONLY CHAIR CAN TAKE CRV LP PROFIT");
@@ -316,7 +316,7 @@ contract ConvexFedTest is DSTest {
         dola.mint(dolaFaucet, amount);
         //Trade back and forth to create a profit
         dola.approve(address(crvPool), type(uint).max);
-        crv3.approve(address(crvPool), type(uint).max);
+        fraxBP.approve(address(crvPool), type(uint).max);
         uint input = amount;
         for(uint i; i < times; i++){
             uint received = crvPool.exchange(0, 1, input, 1);
