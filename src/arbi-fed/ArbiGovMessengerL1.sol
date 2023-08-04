553c833a754ef93937b7b79f62b7b6f36c3114a3
08xmt <markus.08xmt@gmail.com>
2023-08-04 13:01:01 +0200

    Add allowList and pending-claim logic

--- src/arbi-fed/ArbiGovMessengerL1.sol
+++ src/arbi-fed/ArbiGovMessengerL1.sol
@@ -4,12 +4,15 @@ import {IL1GatewayRouter} from "arbitrum/tokenbridge/ethereum/gateway/IL1Gateway
 import {IInbox} from "arbitrum-nitro/contracts/src/bridge/IInbox.sol";
 
 contract ArbiGovMessengerL1 {
-    // TODO: should we add some timelock mechanism for updating the gov address?
     error OnlyGov();
+    error OnlyAllowed();
+    error OnlyPendingGov();
 
     IL1GatewayRouter public immutable gatewayRouter = IL1GatewayRouter(0x72Ce9c846789fdB6fC1f34aC4AD25Dd9ef7031ef); 
     
     address public gov;
+    address public pendingGov;
+    mapping(address => bool) public allowList;
 
     struct L2GasParams {
         uint256 _maxSubmissionCost;
@@ -24,7 +27,12 @@ contract ArbiGovMessengerL1 {
     }
 
     modifier onlyGov {
-        if (msg.sender != gov) revert OnlyGov();
+         if (msg.sender != gov) revert OnlyGov();
+        _;   
+    }
+
+    modifier onlyAllowed {
+        if (msg.sender != gov && !allowList[msg.sender]) revert OnlyAllowed();
         _;
     }
     
@@ -37,7 +45,7 @@ contract ArbiGovMessengerL1 {
         uint256 _l2CallValue,
         L2GasParams memory _l2GasParams,
         bytes memory _data
-    ) external payable onlyGov() returns (uint256) {
+    ) external payable onlyAllowed() returns (uint256) {
         
         emit MessageSent(_to, _data);
 
@@ -60,7 +68,17 @@ contract ArbiGovMessengerL1 {
         return IInbox(_inbox).depositEth{ value: msg.value }();
     }
 
-    function changeGov(address newGov) external onlyGov {
-        gov = newGov;
+    function setPendingGov(address newPendingGov) external onlyGov {
+        pendingGov = newPendingGov;
+    }
+
+    function claimGov() external { 
+        if(msg.sender != pendingGov) revert OnlyPendingGov();
+        gov = pendingGov;
+        pendingGov = address(0);
+    }
+
+    function setAllowed(address allowee, bool isAllowed) external onlyGov {
+        allowList[allowee] = isAllowed;
     }
 }
