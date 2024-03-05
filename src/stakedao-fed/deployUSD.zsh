23dc638be73738797ef0b1ca0bbee7f11a4240f0
fabiaZ84 <fabiaz84@protonmail.com>
2024-03-05 22:40:43 +0100

    deploy parameters BaoUSD updated and validated

--- src/stakedao-fed/deployUSD.zsh
+++ src/stakedao-fed/deployUSD.zsh
@@ -7,7 +7,6 @@ std="0x73968b9a57c6E53d41345FD57a6E6ae27d6CDB2F"
 vault="0xBA12222222228d8Ba445958a75a0704d566BF2C8"
 bpt="0x7E9AfD25F5Ec0eb24d7d4b089Ae7EcB9651c8b1F"
 balancerVault="0xd9663a5e08f0b3db295c5346c1b52677b7398585"
-baoGauge="0x1A44E35d5451E0b78621A1B3e7a53DFaA306B1D0"
 sdbaousdGauge="0xC6A0B204E28C05838b8B1C36f61963F16eCD64C4"
 rewards="0x633120100e108F03aCe79d6C78Aac9a56db1be0F"
 chair="0xFC69e0a5823E2AfCBEb8a35d33588360F1496a00"
@@ -21,7 +20,7 @@ maxLossTakeProfit=10
 poolId="0x7e9afd25f5ec0eb24d7d4b089ae7ecb9651c8b1f000000000000000000000511"
 
 # Concatenate the constructor arguments into a single string
-constructorArgs="($dola, $bal, $std, $vault, $bpt, $balancerVault, $baoGauge, $rewards, $chair, $guardian, $gov)"
+constructorArgs="($dola, $bal, $std, $vault, $bpt, $balancerVault, $sdbaousdGauge, $rewards, $chair, $guardian, $gov)"
 
 # Run the forge create command
 forge create \
